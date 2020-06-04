7b68ac749f7603d2e4ef3ff6c98c3d35fba5706b
Jaime Hernandez Cruz <jaime.cruz@globant.com>
2020-06-03 21:09:33 -0500

    [Feature] Adding Subscribe component

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a3c6782e497e55d90633";
+/******/ 	var hotCurrentHash = "9e399f6f6e0408149d15";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
