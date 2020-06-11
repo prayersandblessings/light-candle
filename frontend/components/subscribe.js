e9fc1b2a9f3789948adaf11edf99427235884d8c
dnnycortes <daniela.munozc@gmail.com>
2020-06-11 14:26:51 -0500

    Adding styles to Subscribe Components

--- frontend/components/subscribe.js
+++ frontend/components/subscribe.js
@@ -7,20 +7,22 @@ const SubscribeComponent = () => {
     alert('Submiting');
   }
   return (
-    <div>
+    <div className={styles.subscribeComponent}>
       <span>RECEIVE</span>
       <h1>Updates & News</h1>
       <p>
         Join the Prayers & Blessings community and receive our latest updates and news.
       </p>
-      <p>
-        <input type='text' className={styles.input} placeholder='Your name' />
-        <input type='text' className={styles.input} placeholder='Your surname' />
-      </p>
-      <p>
-        <input type='email' className={styles.input} placeholder='Your email' />
-        <button type='submit' className={styles.submitButton} onClick={onSubmit}>Submit</button>
-      </p>
+      <form>
+        <p>
+          <input type='text' placeholder='Your name' />
+          <input type='text' placeholder='Your surname' />
+        </p>
+        <p>
+          <input type='email' placeholder='Your email' />
+          <button type='submit' className="main-button" onClick={onSubmit}>Subscribe</button>
+        </p>
+      </form>
     </div>
   );
 }
