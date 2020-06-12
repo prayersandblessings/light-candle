c725374b98e9a9877174591cb96e6a505282b139
dnnycortes <daniela.munozc@gmail.com>
2020-06-11 23:23:25 -0500

    Adding secondary nav

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -1,5 +1,24 @@
 import Nav from './nav'
 import styles from './layout.module.scss'
+import Link from 'next/link'
+import PAGES from '../constants/routes'
+
+const SecondaryNav = () => (
+  <div className={styles.secondaryNav}>
+    <Link as={PAGES.A_BLESSING_DAY.url} href={PAGES.A_BLESSING_DAY.url}>
+      A Blessing a Day
+    </Link> |
+    <a>Subscribe</a>
+    <ul>
+      <li>
+        <a href="" className={styles.facebook}>Facebook</a>
+      </li>
+      <li>
+        <a href="" className={styles.twitter}>Twitter</a>
+      </li>
+    </ul>
+  </div>
+)
 
 const Layout = ({ children, categories }) => (
   <> 
@@ -7,6 +26,7 @@ const Layout = ({ children, categories }) => (
       {children}
     </div>
     <Nav categories={categories} />
+    <SecondaryNav />
   </>
 )
 
