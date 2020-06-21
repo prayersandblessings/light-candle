3606e79f8e8bbb08a1d9d1555c37d05dc8d41c09
dnnycortes <daniela.munozc@gmail.com>
2020-06-21 11:55:58 -0500

    Frontend - Unify titles's size in all pages

--- frontend/pages/a-blessing-a-day.js
+++ frontend/pages/a-blessing-a-day.js
@@ -7,8 +7,8 @@ const ABlessingADay = () => {
   return (
     <Layout>
       <div className={styles.container}>
-        <span className={styles.caption + ' caption'}>A Blessing</span>
-        <h2 className={styles.title}>A Day</h2>
+        <span class="caption">A Blessing</span>
+        <h2 className={styles.title + ' title'}>A Day</h2>
         <p className={styles.paragraph}>A blessing a day is a word that reflects a noble quality. You will receive this word every day, and by implementing it into your life you will add to the positive energy in the world.</p>
         <form className={styles.subscribeForm}>
           <input type="email" placeholder="Your email" class="secondary"></input>
--- frontend/pages/about-us.js
+++ frontend/pages/about-us.js
@@ -9,7 +9,7 @@ const AboutUs = () => {
     <Layout backgroundState = 'color'>
         <div className={styles.container}>
           <span className={styles.caption + ' caption'}>About</span>
-          <h2 className={styles.title}>Who we are</h2>
+          <h2 className={styles.title + ' title'}>Who we are</h2>
           <div className={styles.aboutUsText}>
             <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem veritatis.</p>
