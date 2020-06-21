3b7f4d66d187dd44a03ca16d5fee671394f70493
Jaime Hernandez Cruz <jhdezcruz@gmail.com>
2020-06-21 17:45:16 -0500

    FEATURE - BACKEND - Updating pg_prayers and blessings

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,9 +5,9 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'lightcandle'),
+        database: env('DATABASE_NAME', 'df23haqh1erco0'),
         username: env('DATABASE_USERNAME', 'postgres'),
         password: env('DATABASE_PASSWORD', 'password'),
         ssl: env.bool('DATABASE_SSL', false),
