b443a59920dbb81c9eb28d24c06922245f17e961
dnnycortes <daniela.munozc@gmail.com>
2020-06-21 11:24:15 -0500

    Frontend - Removing documentary button in about us page

--- frontend/pages/about-us.js
+++ frontend/pages/about-us.js
@@ -15,7 +15,6 @@ const AboutUs = () => {
             <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem veritatis.</p>
           </div>
           <img src="/about-us.jpg" className={styles.aboutUsImage}></img>
-          <button className={styles.documentaryButton + ' secondary-button'}>About the documentary</button>
           <div className={styles.aboutUsDescription}>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem veritatis.</p>
