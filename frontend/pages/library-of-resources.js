a089383d65df7b70fe2e5cb9f4d2dea36e38c55a
Jaime Hernandez Cruz <jhdezcruz@gmail.com>
2020-06-23 00:32:50 -0500

    FRONTEND - adding dropdown and fixing image in library of resources

--- frontend/pages/library-of-resources.js
+++ frontend/pages/library-of-resources.js
@@ -10,6 +10,7 @@ const LibraryOfResources = () => {
   let [resources, setResources] = useState([]);
   let [categories, setCategories] = useState([]);
   let [categorySelected, setCategory] = useState('');
+  let [libraryElements, serLibraryElements] = useState([]);
   const categoriesSectionRef = useRef('');
   const [hasOverlay, setHasOverlay] = useState(false)
 
@@ -26,8 +27,8 @@ const LibraryOfResources = () => {
     alert('Share category', title);
   }
 
-  const handleResourcesChanges = (resources, categories) => {
-    setResources(resources);
+  const handleResourcesChanges = (libResources, categories) => {
+    setResources(libResources);
     setCategories(categories);
   }
 
@@ -42,26 +43,35 @@ const LibraryOfResources = () => {
       handleResourcesChanges([], []);
     })
   },[]);
-  const images = [{ title: 'Image', content:'', author:'' }, { title: 'Image', content:'', author:'' }, { title: 'Image', content:'', author:'' } ];
-  const filteredResources = resources.filter(({
-    resource_category: { title: category }
-  }, index) => {
-    if(category === categorySelected) {
-      return true;
-    }
-    if(categorySelected === '' && index < 5){
-      return true;
-    }
-    return false;
-  });
 
-  images.forEach((image, index) => {
-    if(index % 3 === 0 ) {
-      filteredResources.splice(index+3,0, image);
-    }
-  });
+  useEffect(()=>{
+    const images = [{ title: 'Image', content: '', author: '' }, { title: 'Image', content: '', author: '' }, { title: 'Image', content: '', author: '' }];
 
-  const libraryElements = filteredResources;
+    const filteredResources = resources.filter(({
+      resource_category: { title: category }
+    }, index) => {
+      if (category === categorySelected) {
+        return true;
+      }
+      if (categorySelected === '' && index < 5) {
+        return true;
+      }
+      return false;
+    });
+    let indexResources = 0;
+    let indexImages = 0;
+    while (indexResources < filteredResources.length && indexImages < images.length) { 
+      if (indexResources % 4 === 0 && indexResources !== 0) {
+        filteredResources.splice(indexResources-1, 0, images[indexImages]);
+        indexImages += 1;
+      }
+
+      indexResources += 1;
+    }
+
+    serLibraryElements(filteredResources);
+    console.log('Updating filtered Resources')
+  },[resources, categorySelected]);
 
   return (
     <Layout>
@@ -83,10 +93,10 @@ const LibraryOfResources = () => {
           ))}
         </div>
         { libraryElements.map(
-          ({ title, content, author }) =>
+          ({ title, content, author }, index) =>
             <>
               {title === 'Image' && (
-                <img src="/image-library.jpg" className={styles.prayerImage}/>
+                <img  key={`${title}-${index}`}  src="/image-library.jpg" className={styles.prayerImage}/>
               )}
               {title !== 'Image' && (
                 <div key={`${title}-${author}`} className={styles.prayer}>
