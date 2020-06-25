94e8b56710a4f0b1c0a346597cb72ce9ea02aab8
Jaime Hernandez Cruz <jhdezcruz@gmail.com>
2020-06-24 22:42:02 -0500

    FRONTEND - Adding share buttons and labels on light a candle

--- frontend/components/StayHereQuietly/StayHereQuietly.js
+++ frontend/components/StayHereQuietly/StayHereQuietly.js
@@ -1,6 +1,15 @@
 import React, { useRef, useState, useEffect } from 'react';
 import styles from './StayHereQuietly.module.scss';
 
+import {
+  TwitterShareButton,
+  TwitterIcon,
+  FacebookShareButton,
+  FacebookIcon,
+  WhatsappShareButton,
+  WhatsappIcon
+} from "react-share";
+
 const StayHereQuietly = ({ soundUrl }) => {
   const audioRef = useRef();
   const [isPlaying, setIsPlaying] = useState(true);
@@ -28,6 +37,8 @@ const StayHereQuietly = ({ soundUrl }) => {
     )
   },[])
 
+  const URL = window.location.href;
+
   return (
     <div className={styles.container}>
       <div className={`${styles.showText} ${isShowingText ? '' : styles.fadeOut}`}>
@@ -35,17 +46,49 @@ const StayHereQuietly = ({ soundUrl }) => {
         <h1 className={styles.title + ' title'}>As long as you wish</h1>
       </div>
       <div className={styles.toolBar}>
-        {soundUrl && (
-          <button onClick={disableAudio}>
-            <img src="/icon-audio.svg" width="18px" />
+        {/* {soundUrl && ( */}
+          <div className={styles.icon}>
+            <button onClick={disableAudio}>
+              <img src="/icon-audio.svg" width="18px" />
+            </button>
+            <label className={styles.icon}>
+              Mute
+            </label>
+          </div>
+        {/* )} */}
+        <div className={styles.icon}>
+          <button onClick={openNewWidow}>
+            <img src="/icon-download.svg" width="18px"></img>
           </button>
-        )}
-        <button onClick={openNewWidow}>
-          <img src="/icon-download.svg" width="18px"></img>
-        </button>
-        <button>
+          <label>
+            Download candle
+          </label>
+        </div>
+        <div className={styles.icon}>
           <img src="/icon-share.svg" width="18px"></img>
-        </button>
+          <label>
+            <TwitterShareButton
+              url={URL}
+              title="Send your candle."
+            >
+              <TwitterIcon size={32} round={true} />
+            </TwitterShareButton>
+
+            <FacebookShareButton
+              url={URL}
+              quote="Send your candle."
+            >
+              <FacebookIcon size={32} round={true} />
+            </FacebookShareButton>
+
+            <WhatsappShareButton
+              url={URL}
+              title="Send your candle."
+            >
+              <WhatsappIcon size={32} round={true} />
+            </WhatsappShareButton>
+          </label>
+        </div>
       </div>
       {soundUrl && (
         <div className={styles.audio}>
