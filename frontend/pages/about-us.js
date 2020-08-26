30a6881e471e2348b6da970fda43aa3845ac5417
dnnycortes <daniela.munozc@gmail.com>
2020-08-25 22:16:09 -0500

    Frontend - Changing about us html structure and adding final copies and content

--- frontend/pages/about-us.js
+++ frontend/pages/about-us.js
@@ -8,27 +8,37 @@ const AboutUs = () => {
   return (
     <Layout backgroundState = 'color'>
         <div className={styles.container}>
-          <span className={styles.caption + ' caption'}>About</span>
-          <h2 className={styles.title + ' title'}>Who we are</h2>
-          <div className={styles.aboutUsText}>
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem veritatis.</p>
+          <span className={styles.caption + ' caption'}>Our</span>
+          <h2 className={styles.title + ' title'}>Intention</h2>
+          <div className={styles.ourIntention}>
+            <div className={styles.scroll}>
+              <div></div>
+              <span className={'caption'}>Scroll</span>
+            </div>
+            <p> The intention behind the Prayers and Blessings website is for people everywhere to experience peace, inspiration and solace through the power of prayer and the offering of blessings. May people everywhere readily access their own inner strength through cultivating the practice of offering prayers and blessings.</p>
           </div>
-          <img src="/about-us.jpg" className={styles.aboutUsImage}></img>
-          <div className={styles.aboutUsDescription}>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem veritatis.</p>
-            <p>Et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
+          <div className={styles.ourStory}>
+            <div>
+              <h2 className={'title'}>Our Story</h2>
+              <p>During the devastating bushfires in Australia during 2019 and into 2020 we noticed that many communities around the world started to gather and offer their prayers for Australia. We heard stories of the Australian Indigenous people performing corroborees, Buddhists around the world offering mantras, people gathering in churches, temples, mosques to call on a sacred, benevolent power to end the fires through the manifestation of rain. Others less inclined to believe in a divine power, visualised the dowsing of the fires by a widespread falling of gentle, penetrating and nurturing rain.</p>
+            </div>
+            <img src="/about-us.jpg"></img>
+          </div>
+          <div className={styles.complementary}>
+            <p>We observed that many people who engaged in such activities discovered a haven of solace within. From this place they were able to share their positive feelings and actions with others, making a difference to people, wildlife and the environment. Widespread rains fell across many of the firegrounds in January 2020 and by March 31st 2020 the last of the fires were extinguished.</p>
+            <p>We had long held the thought that a place accessible to everyone, where people could focus their sweet intentions no matter what the reason was needed in the world.</p>
+            <p>This became more relevant as the fires came to an end and the world was gripped by Covid-19 and all public places of ritual and prayer were closed due to lockdowns.</p>
+            <p>We decided to create this virtual sacred hub to ensure in all places, times and circumstances, people could come together as a universal community and make offerings of prayers and blessings. And in doing so, contribute to a vibration of peace, love and positive action around the world.</p>
             <div className="divider"></div>
             <Link
               as={`${PAGES.LIGHT_A_CANDLE.url}`}
               href={PAGES.LIGHT_A_CANDLE.url}
             >
               <button>
-                <span className="caption">Light</span>
+                <span className={'caption'}>Light</span>
                 <span className={styles.lightCandleButton}>
                   A candle 
-                  <img src='/icon-arrow-right.svg' width='36px'></img>
+                  <img src='/icon-arrow-right.svg' width='52px'></img>
                 </span>
               </button>
             </Link>
