42247782faddbfbba9f675b28a7b44e11c72e8be
dnnycortes <daniela.munozc@gmail.com>
2020-08-28 17:43:16 -0500

    Frontend - Removing form from receive a blessing and adding SubscribeComponent, cause it's the same form

--- frontend/pages/receive-a-blessing/index.js
+++ frontend/pages/receive-a-blessing/index.js
@@ -1,21 +1,12 @@
-import React, {useState} from 'react'
+import React from 'react'
 import Layout from '../../components/layout'
 import styles from './index.module.scss'
-import Checkbox from '../../components/form/Checkbox'
 import PAGES from '../../constants/routes';
+import SubscribeComponent from '../../components/subscribe'
+
 
 
 const ReceiveABlessing = () => {
-  const [blessingDayAccepted, setBlessingDayAccepted] = useState(false)
-  const [regularMailingAccepted, setRegularMailingAccepted] = useState(false)
-  
-  const handleBlessingDayAccepted = (value) => {
-    setBlessingDayAccepted(value)
-  }
-
-  const handleRegularMailingAccepted = (value) => {
-    setRegularMailingAccepted(value)
-  }
 
   return (
     <Layout classNameSection={PAGES.RECEIVE_A_BLESSING.className}>
@@ -23,29 +14,7 @@ const ReceiveABlessing = () => {
         <span className="caption">Receive</span>
         <h2 className={styles.title + ' title'}>A blessing</h2>
         <p>Subscribe to receive a daily blessing in the form of an inspiring word to contemplate represented by an uplifting visual image. You can contemplate the blessing throughout the day or share it with whoever you wish.</p>
-        <form className={styles.subscribeForm}>
-          <input type="text" placeholder="Your Name" className="secondary"></input>
-          <input type="text" placeholder="Your Surname" className="secondary"></input>
-          <input type="email" placeholder="Your Email" className="secondary"></input>
-          <div>
-              <Checkbox
-                id="subscribe-blessing-day"
-                label={ 'Subscribe me to A Blessing a Day'}
-                checked = { blessingDayAccepted }
-                onClick = { handleBlessingDayAccepted }
-              />
-              <Checkbox
-                id="regular-mailing"
-                label={ 'Subscribe me to the regular mailing list'}
-                checked = { regularMailingAccepted }
-                onClick = { handleRegularMailingAccepted }
-              />
-          </div>
-          <button type="submit" className="next-button">
-            <span className='caption'>Subscribe me</span>
-            <img src="/icon-arrow-right.svg" width="52px"></img>
-          </button>
-        </form>
+        <SubscribeComponent/>
       </div>
     </Layout>
   )
