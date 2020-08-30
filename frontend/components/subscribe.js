8af657635f8c69e1fea576fa38651c68191f69f0
dnnycortes <daniela.munozc@gmail.com>
2020-08-30 17:36:16 -0500

    Frontend - Add a two step page on subscribe page to thank the user after form is submitted

--- frontend/components/subscribe.js
+++ frontend/components/subscribe.js
@@ -1,9 +1,14 @@
 
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import styles from './subscribe.module.scss'
 import Checkbox from '../components/form/Checkbox'
 
-const SubscribeComponent = () => {
+const SECTIONS = {
+  STEP1: 'FORM',
+  STEP2: 'SUBMITED',
+}
+
+const SubscribeComponent = ({onSubscribe}) => {
 
   const onSubmit = () => {
     alert('Submiting');
@@ -20,31 +25,74 @@ const SubscribeComponent = () => {
     setRegularMailingAccepted(value)
   }
 
+  const inputName = useRef(null);
+  const inputEmail = useRef(null);
+  const inputSurname = useRef(null);
+
+  const [showSection, setSection] = useState(SECTIONS.STEP1);
+
+  const handleSumbitForm = () => {
+    setSection(SECTIONS.STEP2);
+    onSubscribe();
+  }
+
+  const onHandleSubmit = (event) => {
+    event.preventDefault();
+    const { current : { value: name = '' }} = inputName;
+    const { current : { value: surname = '' }} = inputSurname;
+    const { current : { value: email = '' }} = inputEmail;
+    console.log( name, surname, email);
+    handleSumbitForm();
+  }
+
   return (
     <div className={styles.subscribeComponent}>
-      <form>
-        <input type="text" placeholder="Your Name" className="secondary"></input>
-        <input type="text" placeholder="Your Surname" className="secondary"></input>
-        <input type="email" placeholder="Your Email" className="secondary"></input>
-        <div>
-          <Checkbox
-            id="subscribe-blessing-day"
-            label={ 'Subscribe me to A Blessing a Day'}
-            checked = { blessingDayAccepted }
-            onClick = { handleBlessingDayAccepted }
+      {showSection === SECTIONS.STEP1 && (
+        <form onSubmit={onHandleSubmit}>
+          <input
+            type="text"
+            placeholder="Your Name"
+            name="Name"
+            className="secondary"
+            ref={inputName}
           />
-          <Checkbox
-            id="regular-mailing"
-            label={ 'Subscribe me to the regular mailing list'}
-            checked = { regularMailingAccepted }
-            onClick = { handleRegularMailingAccepted }
+          <input
+            type="text"
+            placeholder="Your Surname"
+            surname="Surname"
+            className="secondary"
+            ref={inputSurname}
           />
-        </div>
-        <button type="submit" className="next-button" onClick={onSubmit}>
-          <span className='caption'>Subscribe me</span>
-          <img src="/icon-arrow-right.svg" width="52px"></img>
-        </button>
-      </form>
+          <input
+            type="email"
+            placeholder="Your Email"
+            email="Email"
+            className="secondary"
+            ref={inputEmail}
+          />
+          <div>
+            <Checkbox
+              id="subscribe-blessing-day"
+              label={ 'Subscribe me to A Blessing a Day'}
+              checked = { blessingDayAccepted }
+              onClick = { handleBlessingDayAccepted }
+            />
+            <Checkbox
+              id="regular-mailing"
+              label={ 'Subscribe me to the regular mailing list'}
+              checked = { regularMailingAccepted }
+              onClick = { handleRegularMailingAccepted }
+            />
+          </div>
+          <button type="submit" className="next-button" onClick={onSubmit}>
+            <span className='caption'>Subscribe me</span>
+            <img src="/icon-arrow-right.svg" width="52px"></img>
+          </button>
+        </form>
+      )}
+      {showSection === SECTIONS.STEP2 && (
+        <p className={styles.thanksText}>Thank you for subscribing!</p>
+      )}
     </div>
   );
 }
