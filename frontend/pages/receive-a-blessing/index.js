92f74a9a85cd00c51e025dd22ae761df60b4890d
dnnycortes <daniela.munozc@gmail.com>
2020-08-30 19:59:41 -0500

    Frontend - Adding thank you page after submiting form in Receive a blessing page

--- frontend/pages/receive-a-blessing/index.js
+++ frontend/pages/receive-a-blessing/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../../components/layout'
 import styles from './index.module.scss'
 import PAGES from '../../constants/routes';
@@ -8,13 +8,28 @@ import SubscribeComponent from '../../components/subscribe'
 
 const ReceiveABlessing = () => {
 
+  const [showTitle, setShowTitle] = useState(true);
+
+  const handleSubscribe = () => {
+    setShowTitle(false);
+  }
+
   return (
     <Layout classNameSection={PAGES.RECEIVE_A_BLESSING.className}>
       <div className={styles.container}>
         <span className="caption">Receive</span>
         <h2 className={styles.title + ' title'}>A blessing</h2>
-        <p>Subscribe to receive a daily blessing in the form of an inspiring word to contemplate represented by an uplifting visual image. You can contemplate the blessing throughout the day or share it with whoever you wish.</p>
-        <SubscribeComponent/>
+        { showTitle && (
+          <>
+            <p>Subscribe to receive a daily blessing in the form of an inspiring word to contemplate represented by an uplifting visual image. You can contemplate the blessing throughout the day or share it with whoever you wish.</p>
+          </>
+        )}
+        <SubscribeComponent onSubscribe={handleSubscribe}/>
+        { !showTitle && (
+          <>
+            <p>Your first blessing will arrive to your inbox shortly.</p>
+          </>
+        )}
       </div>
     </Layout>
   )
