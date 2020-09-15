11dcbc8f3b837f5054ecdc82533582c1abded1a7
dnnycortes <daniela.munozc@gmail.com>
2020-09-15 00:09:43 -0500

    Frontend - Adding meta tags in layout component

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Head from 'next/head'
 import Nav from './nav'
 import TopNav from './TopNav'
 import styles from './layout.module.scss'
@@ -18,6 +19,14 @@ const Layout = ({ children, classNameSection = 'default' }) => {
 
   return (
     <>
+      <Head>
+        <title>Prayers and blessings</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content="The ritual of lighting a candle is ancient and sacred. When we light a candle, we can connect with the light in our own heart. This ritual is a prayer in itself."/>
+        <meta property="og:title" content="Prayers and blessings"/>
+        <meta property="og:description" content="The ritual of lighting a candle is ancient and sacred. When we light a candle, we can connect with the light in our own heart. This ritual is a prayer in itself."/>
+        <meta property="og:image" content="https://images.unsplash.com/photo-1528351655744-27cc30462816?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"/>
+      </Head>
       <TopNav />
       {classNameSection !== "video" && (
         <>
