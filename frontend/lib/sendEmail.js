abd7414e8a6a0a91fbbe7d02a9f199a04d636ddc
dnnycortes <daniela.munozc@gmail.com>
2020-09-15 00:31:35 -0500

    Frontend - Adding senEmail file with send a prayer mailing template

---
+++ frontend/lib/sendEmail.js
@@ -0,0 +1,40 @@
+import fetch from 'node-fetch'
+
+const SENDGRID_API = process.env.SENDGRID_API;
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+
+const sendEmail = async ({ name, email }) => {
+    const result = await fetch(SENDGRID_API, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${SENDGRID_API_KEY}`
+        },
+        body: JSON.stringify({
+          personalizations: [
+            {
+              to: [
+                {
+                  email
+                }
+              ],
+              subject: 'Demo success :)'
+            }
+          ],
+          from: {
+            email: 'noreply@demo.com',
+            name: 'Test SendGrid'
+          },
+          content: [
+            {
+              type: 'text/html',
+              value: `Congratulations <b>${name}</b>, you just sent an email with sendGrid`
+            }
+          ]
+        })
+    });
+    console.log(result);
+}
+
+export { sendEmail };
+© 2020 GitHub, Inc.
