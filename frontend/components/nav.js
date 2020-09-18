c3b801e6e91f75721800216f6c1844b56466a0ed
Jaime Hernández Cruz <jhdezcruz@gmail.com>
2020-09-18 00:28:09 -0500

    FEATURE - Fixing menu

--- frontend/components/nav.js
+++ frontend/components/nav.js
@@ -13,24 +13,26 @@ const Nav = ({isOpen, handleClose, handleOpen}) => {
         </button>
       )}
       {isOpen && (
-        <div className={styles.open}>
-          <button onClick={handleClose}>
-            <img src="/icon-close.svg" width="20px" height="20px"></img>
-          </button>
-          <div className={styles.navContainer}>
-            <ul className={styles.navbar}>
-              {ARRAY_PAGES.map((page) => {
-                return (
-                  <li key={page.url}>
-                    <Link as={`${page.url}`} href={page.url}>
-                      <a onClick={handleClose}>{page.name}</a>
-                    </Link>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div className={styles.divider}></div>
+        <>
+          <div className={styles.menuCloseUp} onClick={handleClose} />
+          <div className={styles.open}>
+            <button onClick={handleClose}>
+              <img src="/icon-close.svg" width="20px" height="20px"></img>
+            </button>
+            <div className={styles.navContainer}>
+              <ul className={styles.navbar}>
+                {ARRAY_PAGES.map((page) => {
+                  return (
+                    <li key={page.url}>
+                      <Link as={`${page.url}`} href={page.url}>
+                        <a onClick={handleClose}>{page.name}</a>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+            <div className={styles.divider}></div>
             <ul className={styles.secondaryNav}>
               {ARRAY_SECONDARY_PAGES.map((page) => {
                 return (
@@ -42,8 +44,11 @@ const Nav = ({isOpen, handleClose, handleOpen}) => {
                 );
               })}
             </ul>
-            <span className={styles.legals}>© 2020 Lapis International Pty Ltd, All rights reserved.</span>
-        </div>
+            <span className={styles.legals}>
+              © 2020 Lapis International Pty Ltd, All rights reserved.
+            </span>
+          </div>
+        </>
       )}
     </>
   );
