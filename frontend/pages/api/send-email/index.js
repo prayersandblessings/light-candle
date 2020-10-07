0ff19903a06cc1439980b01fd332ede39739152f
dnnycortes <daniela.munozc@gmail.com>
2020-10-06 19:18:56 -0500

    Frontend - Changing design in receive a blessing mail

--- frontend/pages/api/send-email/index.js
+++ frontend/pages/api/send-email/index.js
@@ -8,21 +8,21 @@ export default async (req, res) => {
       try {
 
         const emailContent = `
-            <div style="max-width: 100%; background-color: #F8F9FA; padding: 1rem;">
-            <div style="max-width: 620px; margin: 0 auto; padding: 1rem; text-align: center; background-color: #2a1928 !important;">
-                <p style="margin-left: 32px; margin-top: 40px; font-size: 1.5rem; color: #ffffff; text-align: left; font-weight: bold;">Dear ${name}, </p>
-                <p style="margin-left: 32px; margin-bottom: 40px; font-size: 1.25rem;  color: #ffffff; text-align: left;">${senderName} has lit a candle for you:</p>
+            <div style="max-width: 100%; background-color: #e5eaef; padding: 1rem;">
+            <div style="max-width: 620px; margin: 0 auto; padding: 1rem; text-align: center; background-color: #FFFFFF !important;">
+                <p style="margin-left: 32px; margin-top: 40px; font-size: 1.5rem; color: #181f40; text-align: left;">Dear ${name}, </p>
+                <p style="margin-left: 32px; margin-bottom: 40px; font-size: 1.25rem;  color: #181f40; text-align: left;">${senderName} has sent you a blessing:</p>
                 <img style="width: 100%;" src='${urlNEXT}/candle-email.gif' >
-                <p style="font-size: 1.25rem;  color: #ffffff; margin-left: 32px; margin-right: 32px; margin-top: 3rem; line-height: 2rem;">
+                <p style="font-size: 1.25rem;  color: #181f40; margin-left: 32px; margin-right: 32px; margin-top: 3rem; line-height: 2rem;">
                 "${message}"
                 </p>
-                <p style="font-size: 1.25rem; text-align: right; margin-left: 32px; margin-right: 32px; color: #ffffff; margin-bottom: 2rem; font-style: italic;">
+                <p style="font-size: 1.25rem; text-align: right; margin-left: 32px; margin-right: 32px; color: #181f40; margin-bottom: 2rem; font-style: italic;">
                 - ${senderName}
                 </p>
                 <div style="text-align: center;">
-                <a style="border: 1px solid #ffffff !important;
+                <a style="border: 1px solid #181f40 !important;
                     text-decoration:none;
-                    color: #ffffff;
+                    color: #181f40;
                     text-align: center;
                     border-radius: 24px;
                     padding: 10px 32px;
@@ -35,7 +35,7 @@ export default async (req, res) => {
                     Visit Prayers & Blessings
                 </a>
                 </div>
-                <p style="text-align: center; margin-bottom: 60px; color: #ffffff;">
+                <p style="text-align: center; margin-bottom: 60px; color: #181f40; font-size: .75rem;">
                 Copyright © 2020 Prayers & Blessings, All rights reserved.
                 </p>
             </div>
