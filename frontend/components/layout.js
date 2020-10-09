6bd0931fc972f145cc4d83bfce74b0f065cd6da5
Jaime Hernández Cruz <jhdezcruz@gmail.com>
2020-10-08 22:22:28 -0500

    BUGFIX - Fixing safari blink

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -31,15 +31,15 @@ const Layout = ({ children, classNameSection = 'default' }) => {
       </Head>
       <TopNav />
       <>
-        <div className={`${styles.image} ${styles.default} ${classNameSection === 'default' || classNameSection === 'home' ? styles.visible: ''}`} ></div>
-        <div className={`${styles.image} ${styles.ourStory} ${classNameSection === 'ourStory' ? styles.visible: ''}`} ></div>
-        <div className={`${styles.image} ${styles.lightACandle} ${classNameSection === 'lightACandle' ? styles.visible: ''}`} ></div>
-        <div className={`${styles.image} ${styles.receiveAblessing} ${classNameSection === 'receiveAblessing' ? styles.visible: ''}`} ></div>
-        <div className={`${styles.image} ${styles.aPrayerForHumanity} ${classNameSection === 'aPrayerForHumanity' ? styles.visible: ''}`} ></div>
-        <div className={`${styles.image} ${styles.whoWeAre} ${classNameSection === 'whoWeAre' ? styles.visible: ''}`} ></div>
-        <div className={`${styles.image} ${styles.contact} ${classNameSection === 'contact' ? styles.visible: ''}`} ></div>
-        <div className={`${styles.image} ${styles.subscribe} ${classNameSection === 'subscribe' ? styles.visible: ''}`} ></div>
-        <div className={`${styles.image} ${styles.privacyPolicy} ${classNameSection === 'privacyPolicy' ? styles.visible: ''}`} ></div>
+        <img src='/backgrounds/default.jpg' className={`${styles.image} ${styles.default} ${classNameSection === 'default' || classNameSection === 'home' ? styles.visible: ''}`} />
+        <img src='/backgrounds/our-intention.jpg' className={`${styles.image} ${styles.ourStory} ${classNameSection === 'ourStory' ? styles.visible: ''}`} />
+        <img src='/backgrounds/light-a-candle.jpg' className={`${styles.image} ${styles.lightACandle} ${classNameSection === 'lightACandle' ? styles.visible: ''}`} />
+        <img src='/backgrounds/receive-blessing.jpg'className={`${styles.image} ${styles.receiveAblessing} ${classNameSection === 'receiveAblessing' ? styles.visible: ''}`}/>
+        <img src='/backgrounds/prayer-humanity.jpg'className={`${styles.image} ${styles.aPrayerForHumanity} ${classNameSection === 'aPrayerForHumanity' ? styles.visible: ''}`}/>
+        <img src='/backgrounds/who-we-are.jpg'className={`${styles.image} ${styles.whoWeAre} ${classNameSection === 'whoWeAre' ? styles.visible: ''}`} />
+        <img src='/backgrounds/contact.jpg'className={`${styles.image} ${styles.contact} ${classNameSection === 'contact' ? styles.visible: ''}`} />
+        <img src='/backgrounds/subscribe.jpg'className={`${styles.image} ${styles.subscribe} ${classNameSection === 'subscribe' ? styles.visible: ''}`} />
+        <img src='/backgrounds/privacy.jpg'className={`${styles.image} ${styles.privacyPolicy} ${classNameSection === 'privacyPolicy' ? styles.visible: ''}`} />
         <div className={styles.content}>{children}</div>
       </>
 
