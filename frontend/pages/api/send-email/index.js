38fce9e4ed01bb35a1fc6f42a7453625c870f180
dnnycortes <daniela.munozc@gmail.com>
2020-11-24 19:30:36 -0600

    Frontend - Adding max width to sendgrid email templates

--- frontend/pages/api/send-email/index.js
+++ frontend/pages/api/send-email/index.js
@@ -8,70 +8,27 @@ export default async (req, res) => {
       try {
 
         const emailContent = `
-        <table width="100%" bgcolor="#e5eaef" border="0" style="padding: 48px; border-spacing:0;">
-            <tbody bgcolor="#ffffff" style="max-width: 620px;">
-                <tr border="0" bordercolor="#ffffff">
-                    <td style="font-size: 1.5rem; padding-left: 40px; padding-top: 40px; padding-bottom: 32px; color:#181f40;">Dear ${name},</td>
-                </tr>
-                <tr border="0" bordercolor="#ffffff">
-                    <td style="font-size: 1.25rem; padding-left: 40px; padding-bottom: 40px; color:#181f40;">${senderName} has sent you a blessing</td>
-                </tr>
-                <tr>
-                    <td style="padding-left: 20px; padding-right: 20px;">
-                        <img width="100%" src="${urlNEXT}/candle-email.gif" alt="Light a candle image" />
-                    </td>
-                </tr>
-                <tr>
-                    <td style="font-size: 1.25rem; color:#181f40; text-align: center; padding: 52px 40px;">
-                        "${message}"
-                    </td>
-                </tr>
-                <tr>
-                    <td style="text-align: right; padding: 0 40px 40px 40px; font-size: 1.25rem; font-style:italic; color:#181f40;">- ${senderName}</td>
-                </tr>
-                <tr>
-                    <td style="text-align: center;">
-                        <a href="${urlNEXT}"
-                            style="
-                                color: #181f40;
-                                border: 1px solid #181f40;
-                                background: #ffffff;
-                                border-radius: 40px;
-                                font-size: 1.25rem;
-                                padding: 16px 20px;
-                                display: inline-block;
-                                text-decoration: none;
-                            "
-                        > Visit prayers and blessings
-                        </a>
-                    </td>
-                </tr>
-                <tr>
-                    <td style="font-size: .75rem; color:#181f40; text-align: center; padding: 52px 0 80px 0;">
-                        Copyright © 2020 Prayers & Blessings, All rights reserved.
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        `;
-        await sendEmail({ 
-            senderName,
-            name,
-            email,
-            emailContent,
-            subject: `${senderName} has sent you a blessing 🕯️`
-        });
-
-        const emailContentConfirmation = `
-            <table width="100%" bgcolor="#e5eaef" border="0" style="padding: 48px; border-spacing:0;">
+        <div style="background: #e5eaef; padding:48px;">
+            <table width="100%" bgcolor="#e5eaef" border="0" style="border-spacing:0; margin: 0 auto; max-width: 620px;">
                 <tbody bgcolor="#ffffff" style="max-width: 620px;">
                     <tr border="0" bordercolor="#ffffff">
-                        <td style="font-size: 1.25rem; padding-left: 40px; padding-top: 40px; padding-bottom: 40px; color:#181f40;">${senderName}, <br> Your blessing has been sent to: ${name} to the email ${email}</td>
+                        <td style="font-size: 1.5rem; padding-left: 40px; padding-top: 40px; padding-bottom: 32px; color:#181f40;">Dear ${name},</td>
+                    </tr>
+                    <tr border="0" bordercolor="#ffffff">
+                        <td style="font-size: 1.25rem; padding-left: 40px; padding-bottom: 40px; color:#181f40;">${senderName} has sent you a blessing</td>
                     </tr>
                     <tr>
                         <td style="padding-left: 20px; padding-right: 20px;">
                             <img width="100%" src="${urlNEXT}/candle-email.gif" alt="Light a candle image" />
                         </td>
+                    </tr>
+                    <tr>
+                        <td style="font-size: 1.25rem; color:#181f40; text-align: center; padding: 52px 40px;">
+                            "${message}"
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="text-align: right; padding: 0 40px 40px 40px; font-size: 1.25rem; font-style:italic; color:#181f40;">- ${senderName}</td>
                     </tr>
                     <tr>
                         <td style="text-align: center;">
@@ -85,7 +42,6 @@ export default async (req, res) => {
                                     padding: 16px 20px;
                                     display: inline-block;
                                     text-decoration: none;
-                                    margin-top: 40px;
                                 "
                             > Visit prayers and blessings
                             </a>
@@ -98,6 +54,54 @@ export default async (req, res) => {
                     </tr>
                 </tbody>
             </table>
+        </div>
+        `;
+        await sendEmail({ 
+            senderName,
+            name,
+            email,
+            emailContent,
+            subject: `${senderName} has sent you a blessing 🕯️`
+        });
+
+        const emailContentConfirmation = `
+            <div style="background: #e5eaef; padding:48px;">
+                <table width="100%" bgcolor="#ffffff" border="0" style="border-spacing:0; margin: 0 auto; max-width: 620px;">
+                    <tbody bgcolor="#ffffff" style="max-width: 620px;">
+                        <tr border="0" bordercolor="#ffffff">
+                            <td style="font-size: 1.25rem; padding-left: 40px; padding-top: 40px; padding-bottom: 40px; color:#181f40;">${senderName}, <br> Your blessing has been sent to: ${name} to the email ${email}</td>
+                        </tr>
+                        <tr>
+                            <td style="padding-left: 20px; padding-right: 20px;">
+                                <img width="100%" src="${urlNEXT}/candle-email.gif" alt="Light a candle image" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="text-align: center;">
+                                <a href="${urlNEXT}"
+                                    style="
+                                        color: #181f40;
+                                        border: 1px solid #181f40;
+                                        background: #ffffff;
+                                        border-radius: 40px;
+                                        font-size: 1.25rem;
+                                        padding: 16px 20px;
+                                        display: inline-block;
+                                        text-decoration: none;
+                                        margin-top: 40px;
+                                    "
+                                > Visit prayers and blessings
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="font-size: .75rem; color:#181f40; text-align: center; padding: 52px 0 80px 0;">
+                                Copyright © 2020 Prayers & Blessings, All rights reserved.
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         `;
         // Sending confirmation email
         await sendEmail({ 
