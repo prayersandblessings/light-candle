d56a1345f924918167208aca580cf4e127fb4465
dimitriskarakaxas <karakaxasdimitrios@yahoo.com>
2021-10-28 20:24:48 +0300

    Reverting to 38fce9e4ed01bb35a1fc6f42a7453625c870f180

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,51 +1,39 @@
-import React from "react";
-import Link from "next/link";
-import Layout from "../components/layout";
-import PAGES from "../constants/routes";
-import styles from "./index.module.scss";
-import UnderConstruction from "../components/UnderConstruction/UnderConstruction";
+import React from 'react'
+import Link from 'next/link'
+import Layout from '../components/layout'
+import PAGES from '../constants/routes'
+import styles from './index.module.scss'
 
-const UNDER_CONSTRUCTION = "true";
-
-let Home;
-
-export const LinkCandle = ({ url, name }) => {
+export const LinkCandle = ({url, name}) => {
   return (
     <Link as={url} href={url}>
       <a>
         <div className={styles.candleTitle}>
           {name}
-          <img src="/icon-arrow-down.svg" className={styles.iconArrow} />
+          <img src="/icon-arrow-down.svg" className={styles.iconArrow}/>
         </div>
         <img src="/candle.png" className={styles.singleCandle} />
       </a>
     </Link>
-  );
-};
-
-if (UNDER_CONSTRUCTION === "true") {
-  Home = () => {
-    return <UnderConstruction />;
-  };
-} else {
-  Home = () => {
-    return (
-      <Layout classNameSection={PAGES.HOME.className}>
-        <div className={styles.container}>
-          <div className={styles.showText}>
-            <span className="caption">Offer your</span>
-            <h2 className={"title"}>Prayers and Blessings</h2>
-          </div>
-          <div className={styles.candles}>
-            <LinkCandle
-              url={PAGES.LIGHT_A_CANDLE.url}
-              name={PAGES.LIGHT_A_CANDLE.name}
-            />
-          </div>
-        </div>
-      </Layout>
-    );
-  };
+  )
 }
 
-export default Home;
+
+const Home = () => {
+  return (
+    <Layout classNameSection={PAGES.HOME.className}>
+      <div className={styles.container}>
+        <div className={styles.showText}>
+          <span className="caption">Offer your</span>
+          <h2 className={"title"}>Prayers and Blessings</h2>
+        </div>
+        <div className={styles.candles}>
+          <LinkCandle url={PAGES.LIGHT_A_CANDLE.url} name={PAGES.LIGHT_A_CANDLE.name} />
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+
+export default Home
