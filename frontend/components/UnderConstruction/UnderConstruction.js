b12e74cd569bfa57668a4515c97343307e7c60e2
dimitriskarakaxas <karakaxasdimitrios@yahoo.com>
2021-10-28 20:51:14 +0300

    fix: Vercel errors

--- frontend/components/UnderConstruction/UnderConstruction.js
+++ frontend/components/UnderConstruction/UnderConstruction.js
@@ -1,18 +1,13 @@
 import React from "react";
-import Pulse from "react-reveal/Pulse";
 
 import classes from "./UnderConstruction.module.scss";
 
 const UnderConstruction = () => {
   return (
     <div className={classes.content}>
-      <Pulse>
-        <h1>Light to Light</h1>
-      </Pulse>
+      <h1>Light to Light</h1>
 
-      <Pulse>
-        <h2>Prayers and Blessings</h2>
-      </Pulse>
+      <h2>Prayers and Blessings</h2>
 
       <p className={classes["launch-date"]}>Will Launch 2022 As:</p>
 
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,39 +1,48 @@
-import React from 'react'
-import Link from 'next/link'
-import Layout from '../components/layout'
-import PAGES from '../constants/routes'
-import styles from './index.module.scss'
+import React from "react";
+import Link from "next/link";
+import Layout from "../components/layout";
+import PAGES from "../constants/routes";
+import styles from "./index.module.scss";
 
-export const LinkCandle = ({url, name}) => {
+import UnderConstruction from "../components/UnderConstruction/UnderConstruction";
+
+const IsUnderConstruction = "true";
+
+export const LinkCandle = ({ url, name }) => {
   return (
     <Link as={url} href={url}>
       <a>
         <div className={styles.candleTitle}>
           {name}
-          <img src="/icon-arrow-down.svg" className={styles.iconArrow}/>
+          <img src="/icon-arrow-down.svg" className={styles.iconArrow} />
         </div>
         <img src="/candle.png" className={styles.singleCandle} />
       </a>
     </Link>
-  )
-}
-
+  );
+};
 
 const Home = () => {
-  return (
-    <Layout classNameSection={PAGES.HOME.className}>
-      <div className={styles.container}>
-        <div className={styles.showText}>
-          <span className="caption">Offer your</span>
-          <h2 className={"title"}>Prayers and Blessings</h2>
+  if (IsUnderConstruction === "true") {
+    return <UnderConstruction />;
+  } else {
+    return (
+      <Layout classNameSection={PAGES.HOME.className}>
+        <div className={styles.container}>
+          <div className={styles.showText}>
+            <span className="caption">Offer your</span>
+            <h2 className={"title"}>Prayers and Blessings</h2>
+          </div>
+          <div className={styles.candles}>
+            <LinkCandle
+              url={PAGES.LIGHT_A_CANDLE.url}
+              name={PAGES.LIGHT_A_CANDLE.name}
+            />
+          </div>
         </div>
-        <div className={styles.candles}>
-          <LinkCandle url={PAGES.LIGHT_A_CANDLE.url} name={PAGES.LIGHT_A_CANDLE.name} />
-        </div>
-      </div>
-    </Layout>
-  )
-}
+      </Layout>
+    );
+  }
+};
 
-
-export default Home
+export default Home;
