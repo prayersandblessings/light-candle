2b00fc2396e45f667ac9c656f351cd43dcb52bcf
dimitriskarakaxas <karakaxasdimitrios@yahoo.com>
2021-10-28 21:02:31 +0300

    add: Animations & Typography for under_construction page

--- frontend/components/UnderConstruction/UnderConstruction.js
+++ frontend/components/UnderConstruction/UnderConstruction.js
@@ -1,13 +1,18 @@
 import React from "react";
+import Pulse from "react-reveal/Pulse";
 
 import classes from "./UnderConstruction.module.scss";
 
 const UnderConstruction = () => {
   return (
     <div className={classes.content}>
-      <h1>Light to Light</h1>
+      <Pulse>
+        <h1>Light to Light</h1>
+      </Pulse>
 
-      <h2>Prayers and Blessings</h2>
+      <Pulse>
+        <h2>Prayers and Blessings</h2>
+      </Pulse>
 
       <p className={classes["launch-date"]}>Will Launch 2022 As:</p>
 
